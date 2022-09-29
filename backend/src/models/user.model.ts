f2720c1b1907bcfa5295b5536cba8ce5fc6069b8
kiqueangulo <kiqueangulo@gmail.com>
2022-09-29 00:51:43 -0400

    Create interface for userSchema types

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,29 +1,38 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+
+export interface UserDocument extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+  groups: string[];
+}
 
 const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-	},
-	email: {
-		type: String,
-	},
-	password: {
-		type: String,
-	},
-	groups: [{
-		type: Schema.Types.ObjectId,
-		ref: 'Group'
-	}]
-})
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  groups: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Group",
+    },
+  ],
+});
 
-userSchema.pre('save', async function (next) {
-	const hash = await bcrypt.hash(this.password!, 10)
-	this.password = hash
+userSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password!, 10);
+  this.password = hash;
 
-	return next()
-})
+  return next();
+});
 
-const UserModel = mongoose.model('User', userSchema)
+const UserModel = mongoose.model("User", userSchema);
 
-export default UserModel
+export default UserModel;
