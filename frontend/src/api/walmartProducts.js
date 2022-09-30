065792c0721cff947d0d313f3dc46eeb8e74c029
kiqueangulo <kiqueangulo@gmail.com>
2022-09-30 13:53:06 -0400

    Testing imports of env variables

---
+++ frontend/src/api/walmartProducts.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const walmartUrl = "https://api.bluecartapi.com/request";
+const apiKey = process.env.REACT_APP_WALMART_API_KEY;
+
+export function getAllProducts() {
+  console.log(apiKey);
+}
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import FeaturesHome from "./FeaturesHome";
+import { getAllProducts } from "../api/walmartProducts";
 
 function Home() {
-    return (
-        <div>
-            {/* <nav>
+  getAllProducts();
+  return (
+    <div>
+      {/* <nav>
                 <div className="container mx-auto px-6 py-4 flex justify-between items-center">
                     <img src="./images/logo/icons8-kawaii-dinosaur-50.png" style={{width: "40px"}}></img>
                     <div className="">
@@ -27,24 +30,39 @@ function Home() {
                     </div>
                 </div>
             </nav> */}
-            <div className="flex py-20 h-screen bg-gradient-to-b from-[#667eea] to-[#764ba2] justify-center text-center items-center" >
-            {/* <div style={{background: "linear-gradient(#e66465, #9198e5)"}} style={{background: "linear-gradient(#667eea, #764ba2)"}}></div> */}
-                <div className="container mx-auto px-6">
-                    <h2 className="text-4xl font-bold mb-2 text-white">Shop With Your Friends!</h2>
-                    <h3 className="text-2xl mb-8 text-gray-200">Share your shopping list with anyone you want on your next trip.</h3>
-                    <button className=" bg-white text-gray-800 font-bold rounded-full py-4 px-10 shadow-lg uppercase tracking-wider hover:bg-background-dark-color hover:text-light-purple hover:shadow-lg">
-                    <a className="flex mx-auto" href="/sign-up">
-                        Sign Up Now
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                        </svg>
-                    </a>
-                    </button>
-                </div>
-            </div>
-            <FeaturesHome/>
+      <div className="flex py-20 h-screen bg-gradient-to-b from-[#667eea] to-[#764ba2] justify-center text-center items-center">
+        {/* <div style={{background: "linear-gradient(#e66465, #9198e5)"}} style={{background: "linear-gradient(#667eea, #764ba2)"}}></div> */}
+        <div className="container mx-auto px-6">
+          <h2 className="text-4xl font-bold mb-2 text-white">
+            Shop With Your Friends!
+          </h2>
+          <h3 className="text-2xl mb-8 text-gray-200">
+            Share your shopping list with anyone you want on your next trip.
+          </h3>
+          <button className=" bg-white text-gray-800 font-bold rounded-full py-4 px-10 shadow-lg uppercase tracking-wider hover:bg-background-dark-color hover:text-light-purple hover:shadow-lg">
+            <a className="flex mx-auto" href="/sign-up">
+              Sign Up Now
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                />
+              </svg>
+            </a>
+          </button>
         </div>
-    )
+      </div>
+      <FeaturesHome />
+    </div>
+  );
 }
 
 export default Home;
