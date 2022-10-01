2940b45caea1245c94b6721254d735df6031626a
kiqueangulo <kiqueangulo@gmail.com>
2022-10-01 11:02:41 -0400

    Create findUser handler

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 import { CreateUserInput } from "../schema/user.schema";
-import { createUser } from "../service/user.service";
+import { createUser, findUser } from "../service/user.service";
 import log from "../utils/logger";
 
 export async function createUserHandler(
@@ -15,4 +15,11 @@ export async function createUserHandler(
     log.error(error);
     return res.status(409).send(error.message);
   }
+}
+
+export async function findUserHandler(req: Request, res: Response) {
+  const userId = ""; // Store user id
+  const user = await findUser({ _id: userId, validate: true });
+
+  return res.send(user);
 }
