929073c94b121c005d016ae4ee43b8e716e8c523
Caroline Konrad <ckonrad@live.unc.edu>
2022-10-01 12:00:12 -0400

    add img to error page

--- frontend/src/components/Error404.js
+++ frontend/src/components/Error404.js
@@ -12,6 +12,7 @@ function Error404() {
         <div style={{textAlign: 'center'}}>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <img src="./images/logo/icons8-kawaii-dinosaur-100.png" style={{width: "70px", marginLeft: 'auto', marginRight: 'auto'}}></img>
             <button type="button" onClick={goBack}>Go back</button>
         </div>
     );
