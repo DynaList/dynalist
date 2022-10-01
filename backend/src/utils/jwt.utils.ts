ecffc3363e35a07f8e035651cbf1f5800d1ea67c
kiqueangulo <kiqueangulo@gmail.com>
2022-10-01 12:54:59 -0400

    Set up signJwt function

--- backend/src/utils/jwt.utils.ts
+++ backend/src/utils/jwt.utils.ts
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
 
-const publicKey = process.env.PUBLIC_KEY;
-const privateKey = process.env.PRIVATE_KEY;
+const publicKey: string = process.env.PUBLIC_KEY!;
+const privateKey: string = process.env.PRIVATE_KEY!;
 
-function signJwt() {}
+export function signJwt(object: Object, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, privateKey, {
+    ...(options && options),
+    algorithm: "RS256",
+  });
+}
 
 function verifyJwt() {}
