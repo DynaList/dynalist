9e4034773e0060439daea6eeac677671806df5e4
kiqueangulo <kiqueangulo@gmail.com>
2022-10-01 12:25:54 -0400

    Create service to validate user password

--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -17,3 +17,21 @@ export async function createUser(
 export async function findAllUsers() {
   return UserModel.find().lean();
 }
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) return false;
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), "password");
+}
