85a657bb2e2bc80fa5fc80f3eb21e966f03c188c
kiqueangulo <kiqueangulo@gmail.com>
2022-10-01 13:02:33 -0400

    Create access token

--- backend/src/controller/session.controller.ts
+++ backend/src/controller/session.controller.ts
@@ -2,15 +2,20 @@ import { Request, Response } from "express";
 
 import { validatePassword } from "../service/user.service";
 import { createSession } from "../service/session.service";
+import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserHandler(req: Request, res: Response) {
   const user = await validatePassword(req.body);
 
   if (!user) return res.status(401).send("Invalid email or password.");
 
-  const session = createSession(user._id, req.get("userAgent") || "");
+  const session = await createSession(user._id, req.get("userAgent") || "");
 
   // Create an access token
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: `${process.env.ACCESS_TOKEN_TTL}` } // 15 minutes
+  );
   // Create a refresh token
   // Return access and refresh tokens
 }
--- backend/src/models/session.model.ts
+++ backend/src/models/session.model.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { UserDocument } from "./user.model";
 
-export interface SchemaDocument extends mongoose.Document {
+export interface SessionDocument extends mongoose.Document {
   user: UserDocument["_id"];
   valid: boolean;
   userAgent: string;
@@ -24,6 +24,6 @@ const sessionSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const SessionModel = mongoose.model("Session", sessionSchema);
+const SessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
 
 export default SessionModel;
