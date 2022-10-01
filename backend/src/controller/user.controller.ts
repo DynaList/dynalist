bbc62b2a37a0c5404fd56c49ebc87e0f1576df72
Werefox <jack@wrightus.com>
2022-10-01 14:15:40 -0400

    Moved the user seed function to the user controlleR

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { CreateUserInput } from "../schema/user.schema";
 import { createUser, findAllUsers } from "../service/user.service";
 import log from "../utils/logger";
+import { userSeedData } from "../utils/seedData";
 
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput["body"]>,
@@ -21,4 +22,23 @@ export async function findAllUsersHandler(req: Request, res: Response) {
   const user = await findAllUsers();
 
   return res.send(user);
+}
+
+export async function seedUsersHandler(req: Request, res: Response) {
+    // Should authenticate this, check if the user is an admin and if not just show a 404 page
+    let count = req.params.count
+
+    const data = userSeedData(Number(count))
+    const results = []
+  
+    // Save each item individually to make sure we get the middleware running
+    // Otherwise the passwords will be stored as plaintext
+    for (let i = 0; i < data.length; i++) {
+      results.push(await data[i].save())
+    }
+  
+    res.status(201).json({
+      message: 'Seeded data',
+      results: results
+    })
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router, Request, Response } from "express";
-import { userSeedData } from "../utils/seedData";
 import {
   createUserHandler,
   findAllUsersHandler,
+  seedUsersHandler,
 } from "../controller/user.controller";
 
 const userRouter = Router();
@@ -13,24 +13,7 @@ userRouter.get("/", (req: Request, res: Response) => {
 
 userRouter.post("/new" /*, middleware, */, createUserHandler);
 
-userRouter.get('/seeddata/:count', async (req, res) => {
-  // Should authenticate this, check if the user is an admin and if not just show a 404 page
-  let count = req.params.count
-
-  const data = userSeedData(Number(count))
-  const results = []
-
-  // Save each item individually to make sure we get the middleware running
-  // Otherwise the passwords will be stored as plaintext
-  for (let i = 0; i < data.length; i++) {
-    results.push(await data[i].save())
-  }
-
-  res.json({
-    message: 'Seeded data',
-    results: results
-  })
-})
+userRouter.get('/seeddata/:count', seedUsersHandler)
 
 userRouter.get("/all", findAllUsersHandler);
 
