ba3d0896ff42729c708ccd485820a0a51eb98e5d
kiqueangulo <kiqueangulo@gmail.com>
2022-10-03 05:10:00 -0400

    Create route to start a session

--- backend/src/routes/session.routes.ts
+++ backend/src/routes/session.routes.ts
@@ -1,9 +1,11 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 
+import validate from "../middleware/validateResource";
+import { createSessionSchema } from "../schema/session.schema";
 import { createSessionHandler } from "../controller/session.controller";
 
 const sessionRouter = Router();
 
-sessionRouter.post("/", createSessionHandler);
+sessionRouter.post("/", validate(createSessionSchema), createSessionHandler);
 
 export default sessionRouter;
