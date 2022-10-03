fb8831ffdda7b89d037a437a1f723693f787256d
kiqueangulo <kiqueangulo@gmail.com>
2022-10-03 05:24:25 -0400

    Create handler to get all sessions

--- backend/src/controller/session.controller.ts
+++ backend/src/controller/session.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 import { validatePassword } from "../service/user.service";
-import { createSession } from "../service/session.service";
+import { createSession, findSessions } from "../service/session.service";
 import { signJwt } from "../utils/jwt.utils";
 
 export async function createSessionHandler(req: Request, res: Response) {
@@ -25,4 +25,11 @@ export async function createSessionHandler(req: Request, res: Response) {
 
   // Return access and refresh tokens
   return res.send({ accessToken, refreshToken });
+}
+
+export async function getUserSessionsHandler(req: Request, res: Response) {
+  const userId = res.locals.user._id;
+  const sessions = await findSessions({ user: userId, valid: false });
+
+  return res.send(sessions);
 }
