6c23e923a4a58f7f4fa44f8aeacc8df9de74b392
Werefox <jack@wrightus.com>
2022-10-03 19:57:49 -0400

    Filled out missing user routes

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-
 import { CreateUserInput } from "../schema/user.schema";
-import { createUser, findAllUsers } from "../service/user.service";
+import { createUser, deleteUser, editUser, findAllUsers, findUser } from "../service/user.service";
 import log from "../utils/logger";
 import { userSeedData } from "../utils/seedData";
 
+// create one
 export async function createUserHandler(
   req: Request<{}, {}, CreateUserInput["body"]>,
   res: Response
@@ -18,12 +18,54 @@ export async function createUserHandler(
   }
 }
 
+// find one
+export async function findUserHandler(req: Request, res: Response) {
+  try {
+    const user = await findUser(req.params.id)
+    if (user === null) {
+      return res.status(404).send({ message: 'Error: Could not find user' })
+    }
+    return res.status(200).send(user)
+  } catch (error: any) {
+    log.error(error)
+    return res.status(404).send(error.message)
+  }
+}
+
+// edit one
+export async function editUserHandler(req: Request, res: Response) {
+	try {
+		const updatedUser = await editUser(req.params.id, req.body)
+		return res.status(200).send(updatedUser)
+	} catch (error: any) {
+		log.error(error)
+		return res.status(400).send(error.message)
+	}
+}
+
+// delete one
+export async function deleteUserHandler(req: Request, res: Response) {
+	try {
+		const success = await deleteUser(req.params.id)
+		if (success) {
+			return res.status(200).send({ message: "Successfully deleted user", success: true })
+		} else {
+			return res.status(400).send({ message: "Error deleting user", success: false})
+		}
+	} catch (error: any) {
+		log.error(error)
+		return res.status(400).send(error.message)
+	}
+}
+
+// find all
 export async function findAllUsersHandler(req: Request, res: Response) {
   const user = await findAllUsers();
 
   return res.send(user);
 }
 
+// seed
 export async function seedUsersHandler(req: Request, res: Response) {
     // Should authenticate this, check if the user is an admin and if not just show a 404 page
     let count = req.params.count
--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -3,6 +3,7 @@ import { omit } from "lodash";
 
 import UserModel, { UserDocument } from "../models/user.model";
 
+// new user
 export async function createUser(
   input: DocumentDefinition<Omit<UserDocument, "comparePassword">>
 ) {
@@ -14,10 +15,51 @@ export async function createUser(
   }
 }
 
+// find one
+export async function findUser(id: String): Promise<UserDocument> {
+	try {
+		const user = await UserModel.findById(id).exec()
+		if (user === null) {
+			throw new Error("Could not find user")
+		}
+		return user
+	} catch (error: any) {
+		throw new Error(error)
+	}
+}
+
+// edit one
+export async function editUser(id: String, body: Object): Promise<UserDocument> {
+  try {
+    const user = await UserModel.findByIdAndUpdate(id, body, { new: true })
+    if (user === null) {
+      throw new Error("Could not find user to update")
+    }
+    return user
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
+
+// delete one
+export async function deleteUser(id: String): Promise<boolean> {
+	try {
+		const deletedUser = await UserModel.findByIdAndDelete(id)
+		if (deletedUser === null) {
+			throw new Error("Could not find user to delete")
+		}
+		return true
+	} catch (error: any) {
+		throw new Error(error)
+	}
+}
+
+// get all
 export async function findAllUsers() {
   return UserModel.find().lean();
 }
 
+// validate password
 export async function validatePassword({
   email,
   password,
@@ -36,6 +78,3 @@ export async function validatePassword({
   return omit(user.toJSON(), "password");
 }
 
-export async function findUser(query: FilterQuery<UserDocument>) {
-  return UserModel.findOne(query).lean();
-}
