ae673943fbd152a8179eb8d8c0597c4203745fc8
Alissa <alissamak@outlook.com>
2022-10-07 23:24:31 -0400

    added in new list page

--- frontend/src/users/UserTabs.js
+++ frontend/src/users/UserTabs.js
@@ -136,9 +136,9 @@ export default function UserTabs() {
 
                       </li>
                     ))}
-                      {/* <li className="flex">
-                        {Object.values(newObject).map((obj, idx) => ( key={idx} {obj.name}) )}
-                        <a href="/new" className="hover:border-bright-purple hover:border-solid hover:text-bright-purple group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-400 font-medium py-3">
+                      <li className="flex">
+                        {/* {Object.values(newObject).map((obj, idx) => ( key={idx} {obj.name}) )} */}
+                        <a href="/new-list" className="hover:border-bright-purple hover:border-solid hover:text-bright-purple group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-400 font-medium py-3">
                           <svg 
                             className="group-hover:text-bright-purple mb-1 text-slate-400" 
                             width="20" 
@@ -149,14 +149,14 @@ export default function UserTabs() {
                                 d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" 
                               />
                           </svg>
-                          New
+                          New DynaList
                           
                         </a>
-                      </li> */}
+                      </li>
                     
                       <li className="flex">
                         <a
-                          href="/newgroup"
+                          href="/new-group"
                           className="hover:border-bright-purple hover:border-solid hover:text-bright-purple group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-400 font-medium py-3"
                         >
                           <svg
